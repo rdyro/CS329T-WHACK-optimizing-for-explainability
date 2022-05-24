0831d64a861537723e80302c43257e8ab2812b79
Robert Dyro <robert.dyro@gmail.com>
2022-05-23 21:50:17 -0700

    added differentiable LIME code

---
+++ utils.py
@@ -0,0 +1,20 @@
+import pdb
+import torch
+
+
+def lime_fit(X, Yp):
+    X_ = torch.cat([X, torch.ones(X.shape[:-1] + (1,), dtype=X.dtype, device=X.device)], -1)
+    W = torch.linalg.lstsq(X_, Yp)[0]
+    W, b = W[..., :-1, :], W[..., -1, :]
+    return W, b
+
+def sample_around(x0, x_std, N=100, alf=1e-3):
+    opts = dict(device=x0.device, dtype=x0.dtype)
+    return x0[None, ...] + alf * x_std * torch.randn((N,) + tuple(x0.shape), **opts)
+
+
+
+
+
+
+    
